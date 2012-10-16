c2f17811af2b31d7b330151ebd8bb2a6cca2cea0
Tom Haigh <tom.haigh@gmail.com>
2012-10-16 17:34:31 +0100

    add finished events for the players

--- public/js/directives.js
+++ public/js/directives.js
@@ -34,15 +34,16 @@ var youtubeFactory = function() {
 }();
 
 var createYoutubeWrapper = function(container) {
-	var youtubePlayer = null;
-
+	var youtubePlayer = null, notificationEventHandler;
+	
 	return {
 		hide : function() { 
 			$(container).hide(); 
 		},
 		show : function() { $(container).show(); },
 		stop : function() { youtubePlayer.stopVideo(); },
-		play : function(item) {
+		play : function(item, notifier) {
+			notificationEventHandler = notifier;
 			var task = function(player) {
 				var match = /\?v=([^&]+)/i.exec(item.url);
 				player.stopVideo();
@@ -63,6 +64,15 @@ var createYoutubeWrapper = function(container) {
 					events: {
           'onReady': function(evt) {  
 	          	youtubePlayer = evt.target;
+	          	youtubePlayer.addEventListener("onStateChange", function(e) {
+	          		if (e.data == 0) { //ended
+	          			if (notificationEventHandler) {
+	          				notificationEventHandler({
+	          					status : 'finished'
+	          				});
+	          			}
+	          		}
+	          	});
 	          	task(evt.target);
 	          	console.log(youtubePlayer);
 	         	}
@@ -81,13 +91,18 @@ var createSoundCloudWrapper = function(container) {
 		hide : function() { $(container).hide(); },
 		show : function() { $(container).show(); },
 		stop : function() {  if (widget) { widget.pause(); }},
-		play: function(item) { 
+		play: function(item, notifier) { 
 			console.log('play SoundCloud', item); 
 			container.empty();
 			SC.oEmbed(item.url, { auto_play: true }, function(oEmbed) {
 			  console.log( oEmbed);
 				container.html(oEmbed.html);
 				widget = SC.Widget(container.find('iframe').get(0));
+				widget.bind(SC.Widget.Events.FINISH , function() {
+					if(notifier) { 
+						notifier({ status : 'finished' }); 
+					}
+				});
 				console.log(widget);
 			});
 		}
@@ -103,7 +118,7 @@ var createVimeoWrapper = function(container) {
 			if (!videoApi) {  return; }
 				videoApi.api('unload');
 		}, 
-		play: function(item) { 
+		play: function(item, notifier) { 
 			var match = /vimeo.com\/(\d+)/i.exec(item.url);
 			var videoId = match[1];
 			var iframe = $('<iframe/>')
@@ -120,6 +135,11 @@ var createVimeoWrapper = function(container) {
 
 			Froogaloop(iframe[0]).addEvent('ready', function(player_id) {
 				videoApi = Froogaloop(player_id);
+        videoApi.addEvent('finish', function() {
+        	if(notifier) { 
+						notifier({ status : 'finished' }); 
+					}
+        });
         videoApi.api('play');
 			});
 		}
@@ -140,7 +160,6 @@ angular.module('jukeboxDirectives', [])
 						return players[type];
 					}
 					else {
-						//create container div for every player
 						var id = makeid();
 						var elem = $('<div id="' + type +  id + '"></div>');
 						$(element).append(elem);
@@ -169,6 +188,10 @@ angular.module('jukeboxDirectives', [])
 						'Vimeo' : 'vimeo'
 					};
 
+					var notifier = function(message) {
+						console.log("FINISHED");
+					};
+
 					var type = map[item.openGraph.site_name];
 					if (typeof(type) !== "undefined") {
 						player = getPlayer(type);
@@ -183,7 +206,7 @@ angular.module('jukeboxDirectives', [])
 							player.show();
 						}
 						activePlayer = player;
-						player.play(item);
+						player.play(item, notifier);
 					} else {
 						alert('Unsupported item type');
 					}	
