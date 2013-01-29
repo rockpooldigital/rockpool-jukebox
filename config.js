95f5e110684bba4d8c9ab982fa84d2ca27ff0442
atommyb <tom.bartlett@rockpooldigital.com>
2013-01-29 10:22:19 +0000

    clean copy of config

--- config.js
+++ config.js
@@ -1,21 +1,11 @@
 var config = {};
-
-config.FACEBOOK_APP_ID = '237180946408745';
-config.FACEBOOK_APP_SECRET = 'd8f1f1dd169a2252a16f3b672335d71a';
-config.PORT = 8046;
-// config.URL = 'http://192.168.1.144:' + config.PORT;
-// config.URL = 'http://localhost:' + config.PORT;
-config.URL = 'http://192.168.1.144:' + config.PORT;
+config.FACEBOOK_APP_ID = '1';
+config.FACEBOOK_APP_SECRET = '1';
+config.PORT = 80;
+config.URL = 'http://localhost:' + config.PORT;
+config.DB_HOST = 'ds049537.mongolab.com';
+config.DB_USER = 'nodejitsu_rockpooldigital';
+config.DB_PASS = 'ql0j24nni2iql3d9rdv3etum2t';
+config.DB_NAME = 'nodejitsu_rockpooldigital_nodejitsudb3427473830';
+config.DB_PORT = 49537;
 module.exports = config;
-
-
-// config.FACEBOOK_APP_ID = '1';
-// config.FACEBOOK_APP_SECRET = '1';
-// config.PORT = 80;
-// config.URL = 'http://localhost:' + config.PORT;
-// config.DB_HOST = 'ds049537.mongolab.com';
-// config.DB_USER = 'nodejitsu_rockpooldigital';
-// config.DB_PASS = 'ql0j24nni2iql3d9rdv3etum2t';
-// config.DB_NAME = 'nodejitsu_rockpooldigital_nodejitsudb3427473830';
-// config.DB_PORT = 49537;
-// module.exports = config;
